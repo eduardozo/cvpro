44f001bdf4a56aae99423f2784422e6336dea6f4
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-05-11 03:48:47 +0200

    Setting BBDD

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'eu-cdbr-west-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_ec2c9bf5e9a8370'),
+            'username' => env('DB_USERNAME', 'bb16be62b331b4'),
+            'password' => env('DB_PASSWORD', 'dad1844a'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
